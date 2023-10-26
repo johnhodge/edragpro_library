c72d31d1d302d362ff54e7bc3f59fe737bf59458
John <18251637+johnhodge@users.noreply.github.com>
2023-10-25 23:18:23 -0400

    EP-33 exported global link

--- src/app/components/index.d.ts
+++ src/app/components/index.d.ts
@@ -1,4 +1,5 @@
 export { default as GlobalButton } from './button';
 export { default as GlobalNav } from './nav';
 export { default as GlobalCallout } from './callout';
+export { default as GlobalLink } from './link';
 export type { HomepageSectionEntryData } from '../../types';
--- src/app/components/index.js
+++ src/app/components/index.js
@@ -3,11 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.GlobalCallout = exports.GlobalNav = exports.GlobalButton = void 0;
+exports.GlobalLink = exports.GlobalCallout = exports.GlobalNav = exports.GlobalButton = void 0;
 var button_1 = require("./button");
 Object.defineProperty(exports, "GlobalButton", { enumerable: true, get: function () { return __importDefault(button_1).default; } });
 var nav_1 = require("./nav");
 Object.defineProperty(exports, "GlobalNav", { enumerable: true, get: function () { return __importDefault(nav_1).default; } });
 var callout_1 = require("./callout");
 Object.defineProperty(exports, "GlobalCallout", { enumerable: true, get: function () { return __importDefault(callout_1).default; } });
+var link_1 = require("./link");
+Object.defineProperty(exports, "GlobalLink", { enumerable: true, get: function () { return __importDefault(link_1).default; } });
 //# sourceMappingURL=index.js.map
