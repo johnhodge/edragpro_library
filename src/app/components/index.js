1f8bd1ada39ac7ea65da94efaa0790d1aabc8233
John <18251637+johnhodge@users.noreply.github.com>
2023-10-25 23:44:05 -0400

    EP-33 export system page

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.GlobalStandardPage = exports.GlobalLoadingState = exports.GlobalLink = exports.GlobalCallout = exports.GlobalNav = exports.GlobalButton = void 0;
+exports.GlobalSystemPage = exports.GlobalStandardPage = exports.GlobalLoadingState = exports.GlobalLink = exports.GlobalCallout = exports.GlobalNav = exports.GlobalButton = void 0;
 var button_1 = require("./button");
 Object.defineProperty(exports, "GlobalButton", { enumerable: true, get: function () { return __importDefault(button_1).default; } });
 var nav_1 = require("./nav");
@@ -16,4 +16,6 @@ var loading_state_1 = require("./loading-state");
 Object.defineProperty(exports, "GlobalLoadingState", { enumerable: true, get: function () { return __importDefault(loading_state_1).default; } });
 var standard_page_1 = require("../templates/standard-page");
 Object.defineProperty(exports, "GlobalStandardPage", { enumerable: true, get: function () { return __importDefault(standard_page_1).default; } });
+var system_page_1 = require("../templates/system-page");
+Object.defineProperty(exports, "GlobalSystemPage", { enumerable: true, get: function () { return __importDefault(system_page_1).default; } });
 //# sourceMappingURL=index.js.map
---
+++ src/app/templates/system-page.js
@@ -0,0 +1,34 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importStar(require("react"));
+function GlobalSystemPage(props) {
+    return (react_1.default.createElement(react_1.Suspense, { fallback: props.fallback },
+        react_1.default.createElement("div", { className: 'w-96 max-w-full m-auto min-h-[calc(100dvh-154px)] flex flex-col justify-center items-start' },
+            !props.header ? '' : react_1.default.createElement("h2", { className: 'mt-0' }, props.header),
+            react_1.default.createElement("div", { className: 'w-full' }, props.children))));
+}
+exports.default = GlobalSystemPage;
+//# sourceMappingURL=system-page.js.map
