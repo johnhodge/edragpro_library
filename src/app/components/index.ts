2a24a31a6c7e995427f50681ee0a7d0dc62ee341
John <18251637+johnhodge@users.noreply.github.com>
2023-12-06 14:00:31 -0500

    EP-87 export featutre types

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,18 +1,20 @@
-export { default as GlobalButton } from './button';
-export { default as GlobalNav } from './nav';
-export { default as GlobalCallout } from './callout';
-export { default as GlobalLink } from './link';
-export { default as GlobalLoadingState } from './loading-state';
+export type {
+  FeatureData,
+  FeatureEntryData,
+  GlobalButtonData,
+  GlobalCalloutData,
+  HomepageSectionCollectionData,
+  HomepageSectionEntryData,
+  LinkData,
+  MetadataData,
+  TableConfigData,
+  TableData,
+} from '../../types';
 export { default as GlobalStandardPage } from '../templates/standard-page';
 export { default as GlobalSystemPage } from '../templates/system-page';
 export { default as SetMetadata } from '../utils/meta';
-export type {
-  LinkData,
-  MetadataData,
-  TableData,
-  TableConfigData,
-  HomepageSectionEntryData,
-  HomepageSectionCollectionData,
-  GlobalButtonData,
-  GlobalCalloutData,
-} from '../../types';
+export { default as GlobalButton } from './button';
+export { default as GlobalCallout } from './callout';
+export { default as GlobalLink } from './link';
+export { default as GlobalLoadingState } from './loading-state';
+export { default as GlobalNav } from './nav';
